f463467c7972ba25df5ede833e6018b003ba9f09
mwgh05 <melwongh@gmail.com>
2023-09-30 23:48:50 -0600

    Diagrama e imagenes de ventanas

--- codigo/EspeciesRefugios/src/elementos/Refugio.java
+++ codigo/EspeciesRefugios/src/elementos/Refugio.java
@@ -8,13 +8,17 @@ public class Refugio {
     private String provincia;
     private String canton;
     private String senas;
+    private String area;
+    private String horario;
     private List<Especie> especies;
 
-    public Refugio(String nombre, String provincia, String canton, String senas) {
+    public Refugio(String nombre, String provincia, String canton, String senas, String area, String horario) {
         this.nombre = nombre;
         this.provincia = provincia;
         this.canton = canton;
         this.senas = senas;
+        this.area=area;
+        this.horario=horario;
         this.especies = new ArrayList<>();
     }
 
